a3d1a73de7cf269ea0938776c3d064bd3368bef0
Jason Moy <jasonmoy28@gmail.com>
2020-12-30 10:28:59 +0800

    type: <feat> action: <model_summary_with_plot return a list if multiple elements is given in return_result>

--- R/model_summary_with_plot.R
+++ R/model_summary_with_plot.R
@@ -11,7 +11,7 @@
 #' @param id required character or vector of length 1. The nesting variable (e.g. country)
 #' @param graph_label_name optional vector or function. vector of length 2 for two-way interaction graph. vector of length 3 for three-way interaction graph. Vector should be passed in the form of c(response_var, predict_var1, predict_var2, [predict_var3]). Function should be passed as a switch function. See below for an example.
 #' @param estimation_method optional character. See `nlme::lme` for other options
-#' @param return_result optional character. Choose from `short_summary`,`long_summary`, `model`,`plot`,`none`. `none` return nothing. `short_summary` return a short model summary. `long_summary` return the summary using the `base::summary` function. `model` return a lme object. `plot` return the interaction plot.
+#' @param return_result optional vector. Choose from `short_summary`,`long_summary`, `model`,`plot`,`none`. `none` return nothing. `short_summary` return a short model summary. `long_summary` return the summary using the `base::summary` function. `model` return a lme object. `plot` return the interaction plot.
 #' @param print_result  optional vector. Choose from `both`, `long_summary`, `short_summary`, `plot`, `none`. `short_summary` return a short model summary. `long_summary` return the summary using the base::summary. `plot` return the interaction plot.
 #' @param na.action required default to `na.exclude`. See `nlme::lme` for other options
 #' @param cateogrical_var optional vector.
@@ -19,6 +19,7 @@
 #' @param model_performance optional vector. default to c('R2_fixed_effect','R2_full_model'). `R2_full_model` for conditional R^2. `R2_fixed_effect` for marginal R^2. `icc` for intraclass correlation coefficient. Used the `performance::r2()` and `performance::icc()` for model performance
 #'
 #' @return
+#' return a list of all requested items in the order of model, short_summary, long_summary, plot
 #' @export
 #'
 #' @examples
@@ -85,7 +86,7 @@ model_summary_with_plot = function(data, response_variable,
                     na.action = na.action,
                     estimation_method = estimation_method)
 
-  if (!is.null(two_way_interaction_factor)) {
+  if (!is.null(two_way_interaction_factor) & (any(print_result %in% 'plot') | any(return_result %in% 'plot'))) {
     graphing_interaction_factor = two_way_interaction_factor[1:2]
     interaction_plot = two_way_interaction_plot(data = data,
                                                 nlme_object = model,
@@ -93,22 +94,25 @@ model_summary_with_plot = function(data, response_variable,
                                                 cateogrical_var = cateogrical_var,
                                                 graph_label_name = graph_label_name)
 
-    } else if(!is.null(three_way_interaction_factor)){
+    } else if (!is.null(three_way_interaction_factor) & (any(print_result %in% 'plot') | any(return_result %in% 'plot'))) {
     interaction_plot = three_way_interaction_plot(data = data,
                                                   nlme_object = model,
                                                   predict_var_name = three_way_interaction_factor,
                                                   cateogrical_var = cateogrical_var,
                                                   graph_label_name = graph_label_name)
-  } else{
-    error_message = 'Error: object two_way_interaction_factor is not provided'
-  }
+    } else{
+      interaction_plot = NULL
+    }
 
-  model_summary_df = model_summary(nlme_object = model,model_performance = model_performance)
+  if (any(print_result %in% 'short_summary') | any(return_result %in% 'short_summary')) {
+    model_summary_df = model_summary(nlme_object = model,model_performance = model_performance)
+  } else {
+    model_summary_df = NULL
+  }
 
   # Check print result
   if (any(print_result %in% 'short_summary')) {
     print(model_summary_df)
-
   }
 
   if(any(print_result %in% 'long_summary')){
@@ -122,23 +126,27 @@ model_summary_with_plot = function(data, response_variable,
 
 
   # Check return result
-  if(return_result == 'plot'){
-    try(print(interaction_plot))
+  if (any(return_result == 'none')) {
+    return()
+  }
 
-    } else if (return_result == 'short_summary ') {
-       return(model_summary_df)
+  if(any(return_result %in% 'plot')) {
+    return_plot = interaction_plot
+  } else{return_plot = NULL}
 
-      } else if(return_result == 'model'){
-         return(model)
+  if (any(return_result %in% 'short_summary')) {
+    return_short_summary = model_summary_df
+  } else{return_short_summary = NULL}
 
-        } else if(return_result == 'long_summary'){
-          return(summary(model))
+  if(any(return_result %in% 'model')) {
+    return_model = model
+  } else{return_model = NULL}
 
-          } else if (return_result == 'none'){
-            return_result = 'none' # doing something so it won't return null
+  if(any(return_result %in% 'long_summary')) {
+    return_long_summary = summary(model)
+  } else{return_long_summary = NULL}
 
-            } else {
-              return('Error: return_result must be set to short_summary, long_summary, plot, model, or none')
-              }
-
+  return_list = list(return_model,return_short_summary,return_long_summary,return_plot)
+  return_list = return_list[!sapply(return_list,is.null)]
+  return(return_list)
   }
